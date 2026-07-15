b5d530adb2bef5a35597610b30e8d80c1fd1c7ba
MissAi <aihongxu8@gmail.com>
2026-07-15 16:47:33 +0800

    Fix Pages deploy config and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import CustomerReceiptApp from './apps/CustomerReceiptApp'
 import AppOrderingApp from './apps/AppOrderingApp'
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<CustomerReceiptApp />} />
         <Route path="/app-ordering" element={<AppOrderingApp />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
